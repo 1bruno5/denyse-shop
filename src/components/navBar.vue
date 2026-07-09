4e42c45219e9bc84e7411e0cfbdc0a02d3d3f694
1bruno5 <abakundanyeb@gmail.com>
2026-07-09 13:17:19 +0200

    vv

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -27,9 +27,9 @@
       <button class="bg-gray-400 text-white rounded-md px-4 py-1 ml-6 flex"><img src="../assets/photos/icons/user.png" alt="user" class="w-4 h-5"><div class="text-2md"><RouterLink to="/login" class="text-xs mb-4"><div class="justify-center">Account</div></RouterLink></div></button>
     </div>
     
-    <nav class="fixed bottom-0 left-0 right-0 bg-gray-100 text-white border-t flex justify-around items-center py-2 md:hidden z-50">
+    <nav class="fixed bottom-0 left-0 right-0 bg-white border-t flex justify-around items-center py-2 md:hidden z-50">
              <ul class="flex gap-8">
-            <li class="hover:text-red-700 ml-35 "><RouterLink to="/">Home</RouterLink></li>
+            <li class="hover:text-red-700 ml-2 "><RouterLink to="/">Home</RouterLink></li>
             <div class="flex gap-1">
               <li class="hover:text-red-700 ml-4 "><RouterLink to="/products">Products </RouterLink></li>
             </div>
