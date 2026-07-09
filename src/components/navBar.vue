ad4e198a59ebde87760c51e4520386f58a566c7b
1bruno5 <abakundanyeb@gmail.com>
2026-07-09 12:25:40 +0200

    nav

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -21,10 +21,10 @@
       <button class="bg-gray-400 text-white rounded-md px-4 py-1 ml-6 flex gap-3"><img src="../assets/photos/icons/user.png" alt="user" class="w-4 h-5"><div class="text-2md"><RouterLink to="/login" class="text-xs mb-4"><div class="justify-center">Account</div></RouterLink></div></button>
     </div>
 
-        <div class=" flex justify-end ml-10 md:hidden">
+        <div class=" flex justify-end ml-10 md:hidden ml-10">
       <button><img src="../assets/photos/icons/search.png" alt="search" class="w-4 gap-2 "></button>
-      <button><img src="../assets/photos/icons/cart.png" alt="cart" class="w-4  mr-2"></button>
-      <button class="bg-gray-400 text-white rounded-md px-4 py-1 ml-6 flex gap-3"><img src="../assets/photos/icons/user.png" alt="user" class="w-4 h-5"><div class="text-2md"><RouterLink to="/login" class="text-xs mb-4"><div class="justify-center">Account</div></RouterLink></div></button>
+      <button><img src="../assets/photos/icons/cart.png" alt="cart" class="w-4 ml-2  mr-2"></button>
+      <button class="bg-gray-400 text-white rounded-md px-4 py-1 ml-6 flex"><img src="../assets/photos/icons/user.png" alt="user" class="w-4 h-5"><div class="text-2md"><RouterLink to="/login" class="text-xs mb-4"><div class="justify-center">Account</div></RouterLink></div></button>
     </div>
     
    </div>
