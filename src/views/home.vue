ed660e8a8a4a02b505181924cf53f90a7fc5cdd1
1bruno5 <abakundanyeb@gmail.com>
2026-07-09 20:22:16 +0200

    border radius removed

--- src/views/home.vue
+++ src/views/home.vue
@@ -8,7 +8,7 @@
       <p class="font-bold  text-white text-xl sm:text-3xl lg:text-4xl">Your perfect kitchen starts here with<br></p>
         <p class="mt-2 lg:mt-6 font-bold  text-xl sm:text-3xl lg:text-4xl text-white"> quality equipment for every<br></p>
          <p class=" mt-2 lg:mt-6 font-bold text-xl sm:text-3xl lg:text-4xl text-white"> home and business.</p>
-         <button class="bg-red-700 rounded-md px-8 py-2 text-white text-xl mt-20 "><RouterLink to="/products" class="text-base justify-center ml-2 mb-3">Shop Now</RouterLink></button>
+         <button class="bg-red-700  -md px-8 py-2 text-white text-xl mt-20 "><RouterLink to="/products" class="text-base justify-center ml-2 mb-3">Shop Now</RouterLink></button>
       </div>
     </div>
   </div>
@@ -17,7 +17,7 @@
   <section>
     <h1 class="flex justify-center text-gray-700 font-bold text-3xl mt-10  w-full ">Explore our best selling <br> </h1><h1 class="flex justify-center text-gray-700 font-bold text-3xl mt-2  w-full ">collection</h1>
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-10  justify-items-center px-10">
-  <div class="bg-white rounded-xl shadow-xl w-full  mb-10 overflow-hidden  h-[380px] ">
+  <div class="bg-white  -xl shadow-xl w-full  mb-10 overflow-hidden  h-[380px] ">
   <img
    src="../assets/photos/cookingPots/cookingWare.jpg" 
    alt="image"
@@ -27,11 +27,11 @@
    <div class="p-2 mt-2 ml-2">
     <h3 class="font-bold text-base">Cooking Ware</h3>
     <p class="text-sm">70 000 RW</p>
-<button class="bg-teal-600 text-white py-2 px-6 rounded-md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
+<button class="bg-teal-600 text-white py-2 px-6  -md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
    </div>
 </div>
 
-  <div class="bg-white rounded-xl shadow-xl w-full mb-10 overflow-hidden  h-[380px] ">
+  <div class="bg-white  -xl shadow-xl w-full mb-10 overflow-hidden  h-[380px] ">
   <img
    src="../assets/photos/dinnerSet.jpg" 
    alt="image"
@@ -41,11 +41,11 @@
    <div class="p-2 mt-2 ml-2">
     <h3 class="font-bold text-base">Dinner Set</h3>
     <p class="text-sm">60 000 RW</p>
-<button class="bg-teal-600 text-white py-2 px-6 rounded-md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
+<button class="bg-teal-600 text-white py-2 px-6  -md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
    </div>
 </div>
 
-  <div class="bg-white rounded-xl shadow-xl w-full  mb-10 overflow-hidden  h-[380px] ">
+  <div class="bg-white  -xl shadow-xl w-full  mb-10 overflow-hidden  h-[380px] ">
   <img
    src="../assets/photos/greenGrater.jpg" 
    alt="image"
@@ -55,11 +55,11 @@
    <div class="p-2 mt-2 ml-2">
     <h3 class="font-bold text-base">Multi-fuction Grater</h3>
     <p class="text-sm">25 000 RW</p>
-<button class="bg-teal-600 text-white py-2 px-6 rounded-md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
+<button class="bg-teal-600 text-white py-2 px-6  -md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
    </div>
 </div>
 
-  <div class="bg-white rounded-xl shadow-xl w-full  mb-10 overflow-hidden  h-[380px] ">
+  <div class="bg-white  -xl shadow-xl w-full  mb-10 overflow-hidden  h-[380px] ">
   <img
    src="../assets/photos/electronics/chapatiMaker.jpg" 
    alt="image"
@@ -69,7 +69,7 @@
    <div class="p-2 mt-2 ml-2">
     <h3 class="font-bold text-base">Chapati Maker</h3>
     <p class="text-sm">50 000 RW</p>
-<button class="bg-teal-600 text-white py-2 px-6 rounded-md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
+<button class="bg-teal-600 text-white py-2 px-6  -md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
    </div>
 </div>
 
@@ -79,19 +79,19 @@
   
     <h1 class="flex justify-center text-2xl lg:text-3xl font-bold text-gray-700 mb-10 mt-16">Why to choose D Shop ?</h1>
   
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mb-8 justify-items-center px-4 sm:px-8 md:px-16 lg:px-20">
-    <div class="bg-white rounded-xl shadow-2xl   w-full max-w-[350px] mb-10 py-15 ">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 mb-8 justify-items-center px-4 sm:px-8 md:px-16 lg:px-20">
+    <div class="bg-white  -xl shadow-2xl   w-full max-w-[330px] mb-10 py-15 ">
         <h3 class="mt-0 font-bold text-base text-center">Premium Quality</h3>
            <p class=" mt-7 justify-center text-center text-xs">Find everything from cookware and bakeware</p>
            <p class="text-center text-xs"> to appliances and kitchen accessories </p> 
             <p class="text-center text-xs"> in one place.</p>
     </div>
-    <div class="bg-white rounded-xl shadow-2xl w-full max-w-[350px] py-15  mb-10  ">
+    <div class="bg-white  -xl shadow-2xl w-full max-w-[330px] py-15  mb-10  ">
       <h3 class="mt-0 font-bold text-base text-center ">Secure Shopping</h3>
       <p class="text-center mt-7 text-xs">Shop with confidence using safe payment</p>
       <p class="text-center text-xs">  methods and protected checkout.</p>
     </div>
-        <div class="bg-white rounded-xl shadow-2xl w-full max-w-[350px] py-15 mb-10">
+        <div class="bg-white  -xl shadow-2xl w-full max-w-[330px] py-15 mb-10">
       <h3 class="mt-0 font-bold text-base text-center">Fast Delivery</h3>
       <p class="text-center mt-7 text-xs">Receive your kitchen essentials quickly with</p>
       <p class="text-center text-xs"> reliable shipping and secure packaging</p>
@@ -105,67 +105,67 @@
     <h1 class="text-center text-2xl lg:text-3xl text-gray-800 font-bold  ">Our best selling products</h1>
   </div>
   <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4 justify-items-center px-4 md:px-8 lg:px-20 mt-10">
-       <div class="bg-white shadow-2xl rounded-xl   w-full  max-w-[220px] overflow-hidden ">
-           <img src="../assets/photos/electronics/microwave.jpg"
+       <div class="bg-white shadow-2xl  -xl   w-full  max-w-[220px] overflow-hidden ">
+           <img src="../assets/photos/electronics/deepFryer.jpg"
             alt="microwave"
             class="h-[180px] sm:h-[220px] w-full object-cover"
             >
             <div class="p-3">
-                <h3 class="font-bold text-base">Microwave</h3>
-                <p class="text-sm">90 000 RW</p>
-                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
+                <h3 class="font-bold text-base">Deep Fryer</h3>
+                <p class="text-sm">65 00 000 RWF</p>
+                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6  -md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
 
-              <div class="bg-white shadow-2xl rounded-xl   w-full max-w-[220px] overflow-hidden ">
-           <img src="../assets/photos/riceCooker.jpg"
+              <div class="bg-white shadow-2xl  -xl   w-full max-w-[220px] overflow-hidden ">
+           <img src="../assets/photos/spoonForksSet.jpg"
             alt="microwave"
             class="h-[180px] sm:h-[220px] w-full object-cover"
             >
             <div class="p-3">
-                <h3 class="font-bold text-base">Rice Cooker</h3>
-                <p class="text-sm">120 000 RW</p>
-                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
+                <h3 class="font-bold text-base">Spoon & Forks Set</h3>
+                <p class="text-sm">40 000 RWF</p>
+                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6  -md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
 
-              <div class="bg-white shadow-2xl rounded-xl   w-full max-w-[220px] overflow-hidden ">
+              <div class="bg-white shadow-2xl  -xl   w-full max-w-[220px] overflow-hidden ">
            <img src="../assets/photos/blenders/a.jpg"
             alt="microwave"
             class="h-[180px] sm:h-[220px] w-full object-cover"
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">4 in 1 Blender</h3>
-                <p class="text-sm">75 000 RW</p>
-                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
+                <p class="text-sm">75 000 RWF</p>
+                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6  -md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
 
-              <div class="bg-white shadow-2xl rounded-xl   w-full max-w-[220px] overflow-hidden ">
+              <div class="bg-white shadow-2xl  -xl   w-full max-w-[220px] overflow-hidden ">
            <img src="../assets/photos/cookingPots/cooksetWare.jpg"
             alt="microwave"
             class="h-[180px] sm:h-[220px] w-full object-cover object-top"
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">Cooking Pot</h3>
-                <p class="text-sm">60 RW</p>
-                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
+                <p class="text-sm">60 RWF</p>
+                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6  -md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
 
-              <div class="bg-white shadow-2xl rounded-xl   w-full max-w-[220px] overflow-hidden ">
-           <img src="../assets/photos/glasses/g (2).jpg"
+              <div class="bg-white shadow-2xl  -xl   w-full max-w-[220px] overflow-hidden ">
+           <img src="../assets/photos/glasses/wine.jpg"
             alt="microwave"
             class="h-[180px] sm:h-[220px] w-full object-cover"
             >
             <div class="p-3">
-                <h3 class="font-bold text-base">Liquor Glasses</h3>
+                <h3 class="font-bold text-base">Wine Glasses</h3>
                 <p class="text-sm">55 000 RW</p>
-                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
+                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6  -md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
 
-              <div class="bg-white shadow-2xl rounded-xl   w-full max-w-[220px] overflow-hidden ">
+              <div class="bg-white shadow-2xl  -xl   w-full max-w-[220px] overflow-hidden ">
            <img src="../assets/photos/jar.jpg"
             alt="microwave"
             class="h-[180px] sm:h-[220px] w-full object-cover"
@@ -173,31 +173,31 @@
             <div class="p-3">
                 <h3 class="font-bold text-base">Jars</h3>
                 <p class="text-sm">30 000 RW</p>
-                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
+                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6  -md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
 
-              <div class="bg-white shadow-2xl rounded-xl   w-full max-w-[220px] overflow-hidden ">
-           <img src="../assets/photos/waterDispenser.jpg"
+              <div class="bg-white shadow-2xl  -xl   w-full max-w-[220px] overflow-hidden ">
+           <img src="../assets/photos/plastics/MOP.jpg"
             alt="microwave"
             class="h-[180px] sm:h-[220px] w-full object-cover"
             >
             <div class="p-3">
-                <h3 class="font-bold text-base">Water Dispenser</h3>
-                <p class="text-sm">110 000 RW</p>
-                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
+                <h3 class="font-bold text-base">Spin Mop</h3>
+                <p class="text-sm">40 000 RWF</p>
+                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6  -md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
 
-              <div class="bg-white shadow-2xl rounded-xl   w-full max-w-[220px] overflow-hidden ">
-           <img src="../assets/photos/range.jpg"
-            alt="microwave"
+              <div class="bg-white shadow-2xl  -xl   w-full max-w-[220px] overflow-hidden ">
+           <img src="../assets/photos/roomWare/r (10).jpg"
+            alt="closet"
             class="h-[180px] sm:h-[220px] w-full object-cover"
             >
             <div class="p-3">
-                <h3 class="font-bold text-base">Range</h3>
+                <h3 class="font-bold text-base">closet</h3>
                 <p class="text-sm">350 000 RW</p>
-                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
+                <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6  -md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
 
@@ -207,7 +207,7 @@
 <div class="bg-white mb-20 px-4 sm:px-8 lg:px-16 pt-10 sm:pt-16 lg:pt-20">
 <div class="flex flex-col lg:flex-row gap-16 lg:gap-16 ">
     <img src="../assets/photos/about.jpg" alt="about image"
-  class="  w-full max-w-[220px] sm:max-w-[280px] lg:max-w-[340px] rounded-xl lg:rounded-none mx-auto lg:mx-0 object-cover"
+  class="  w-full max-w-[220px] sm:max-w-[280px] lg:max-w-[340px] h-80 lg:h-110   lg: -none mx-auto lg:mx-0 object-cover"
   >
 <div class="">
     <h3 class=" text-xl md:text-2xl lg:text-3xl font-bold text-gray-700 text-center md:text-left">Cook Smarter. Live Better.</h3>
@@ -215,7 +215,7 @@
     <p >At D Shop, we believe every home deserves quality kitchen equipment that makes </p> <p class="mt-2"> cooking enjoyable and effortless. From premium cookware and modern appliances to everyday</p><p class="mt-2">utensils, we carefully  select products that combine durability, style, and performance.</p>
     <p class="mt-8 lg:mt-14">Whether you're a beginner or a professional chef, we're here to help you</p><p> create delicious meals with confidence.</p>
 
-    <button class="bg-red-800 text-white py-3 px-8 rounded-md mt-16 hover:bg-red-700 "><RouterLink to="/about" class="text-sm">Learn More About Us</RouterLink></button>
+    <button class="bg-red-800 text-white py-3 px-8  -md mt-16 hover:bg-red-700 "><RouterLink to="/about" class="text-sm">Learn More About Us</RouterLink></button>
   </div>
 </div>
 </div>
