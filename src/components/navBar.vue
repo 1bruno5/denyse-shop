759bc1932a832192b10a6140b37ebb142ac84ea6
1bruno5 <abakundanyeb@gmail.com>
2026-07-09 12:11:46 +0200

    started creating nav for mobile

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,8 +1,13 @@
 <template>
       <nav class="bg-white p-3">
-   <div class="flex items-center">
+   <div class="hidden md:flex items-center">
     <div class="flex">
-      <h1 class="text-black text-xl font-bold ml-15">LOGO</h1>
+      <h1 class="text-black text-xl font-bold ml-15"><img src="../assets/photos/logo.jpg" alt="logo" class="w-8"></h1>
+    </div>
+    <div class="flex md:hidden items-center gap-4">
+      <button><img src="../assets/photos/icons/search.png" alt="search" class="w-4 gap-8 mr-6"></button>
+      <button><img src="../assets/photos/icons/cart.png" alt="cart" class="w-4 ml-5 mr-8"></button>
+      <button class="bg-gray-400 text-white rounded-md px-4 py-1 ml-6 flex gap-3"><img src="../assets/photos/icons/user.png" alt="user" class="w-4 h-5"><div class="text-2md"><RouterLink to="/login" class="text-xs mb-4"><div class="justify-center">Account</div></RouterLink></div></button>
     </div>
     <div class="text-black flex flex-3 justify-center font-semibold text-xs gap-15">
            <ul class="flex gap-8">
--- src/views/home.vue
+++ src/views/home.vue
@@ -8,7 +8,7 @@
       <p class="font-bold  text-white text-xl sm:text-3xl lg:text-4xl">Your perfect kitchen starts here with<br></p>
         <p class="mt-2 lg:mt-6 font-bold  text-xl sm:text-3xl lg:text-4xl text-white"> quality equipment for every<br></p>
          <p class=" mt-2 lg:mt-6 font-bold text-xl sm:text-3xl lg:text-4xl text-white"> home and business.</p>
-         <button class="bg-red-700 rounded-md px-8 py-2 text-white text-xl mt-23"><RouterLink to="/products" class="text-base justify-center ml-2 mb-3">Shop Now</RouterLink></button>
+         <button class="bg-red-700 rounded-md px-8 py-2 text-white text-xl mt-20 "><RouterLink to="/products" class="text-base justify-center ml-2 mb-3">Shop Now</RouterLink></button>
       </div>
     </div>
   </div>
@@ -39,8 +39,8 @@
    >
 
    <div class="p-2 mt-2 ml-2">
-    <h3 class="font-bold text-base">Cooking Ware</h3>
-    <p class="text-sm">70 000 RW</p>
+    <h3 class="font-bold text-base">Dinner Set</h3>
+    <p class="text-sm">60 000 RW</p>
 <button class="bg-teal-600 text-white py-2 px-6 rounded-md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
    </div>
 </div>
@@ -53,8 +53,8 @@
    >
 
    <div class="p-2 mt-2 ml-2">
-    <h3 class="font-bold text-base">Cooking Ware</h3>
-    <p class="text-sm">70 000 RW</p>
+    <h3 class="font-bold text-base">Multi-fuction Grater</h3>
+    <p class="text-sm">25 000 RW</p>
 <button class="bg-teal-600 text-white py-2 px-6 rounded-md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
    </div>
 </div>
@@ -67,8 +67,8 @@
    >
 
    <div class="p-2 mt-2 ml-2">
-    <h3 class="font-bold text-base">Cooking Ware</h3>
-    <p class="text-sm">70 000 RW</p>
+    <h3 class="font-bold text-base">Chapati Maker</h3>
+    <p class="text-sm">50 000 RW</p>
 <button class="bg-teal-600 text-white py-2 px-6 rounded-md mt-4 hover:bg-red-800"><p class="text-xs">Buy Now</p></button>
    </div>
 </div>
@@ -79,7 +79,7 @@
   
     <h1 class="flex justify-center text-2xl lg:text-3xl font-bold text-gray-700 mb-10 mt-16">Why to choose D Shop ?</h1>
   
-  <div class="grid grid-cols-1 lg:grid-cols-3 gap-2 mb-8 justify-items-center px-4 sm:px-8 md:px-16 lg:px-20">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mb-8 justify-items-center px-4 sm:px-8 md:px-16 lg:px-20">
     <div class="bg-white rounded-xl shadow-2xl   w-full max-w-[350px] mb-10 py-15 ">
         <h3 class="mt-0 font-bold text-base text-center">Premium Quality</h3>
            <p class=" mt-7 justify-center text-center text-xs">Find everything from cookware and bakeware</p>
@@ -104,7 +104,7 @@
   <div class="">
     <h1 class="text-center text-2xl lg:text-3xl text-gray-800 font-bold  ">Our best selling products</h1>
   </div>
-  <div class="grid grid-cols-2 lg:grid-cols-4 gap-x-2 gap-y-4 justify-items-center px-4 md:px-8 lg:px-20 mt-10">
+  <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4 justify-items-center px-4 md:px-8 lg:px-20 mt-10">
        <div class="bg-white shadow-2xl rounded-xl   w-full  max-w-[220px] overflow-hidden ">
            <img src="../assets/photos/electronics/microwave.jpg"
             alt="microwave"
@@ -112,7 +112,7 @@
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">Microwave</h3>
-                <p class="text-sm">70 RW</p>
+                <p class="text-sm">90 000 RW</p>
                 <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
@@ -124,7 +124,7 @@
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">Rice Cooker</h3>
-                <p class="text-sm">70 RW</p>
+                <p class="text-sm">120 000 RW</p>
                 <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
@@ -136,7 +136,7 @@
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">4 in 1 Blender</h3>
-                <p class="text-sm">70 RW</p>
+                <p class="text-sm">75 000 RW</p>
                 <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
@@ -148,7 +148,7 @@
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">Cooking Pot</h3>
-                <p class="text-sm">70 RW</p>
+                <p class="text-sm">60 RW</p>
                 <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
@@ -160,7 +160,7 @@
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">Liquor Glasses</h3>
-                <p class="text-sm">70 RW</p>
+                <p class="text-sm">55 000 RW</p>
                 <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
@@ -172,7 +172,7 @@
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">Jars</h3>
-                <p class="text-sm">70 RW</p>
+                <p class="text-sm">30 000 RW</p>
                 <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
@@ -184,7 +184,7 @@
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">Water Dispenser</h3>
-                <p class="text-sm">70 RW</p>
+                <p class="text-sm">110 000 RW</p>
                 <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
@@ -196,7 +196,7 @@
             >
             <div class="p-3">
                 <h3 class="font-bold text-base">Range</h3>
-                <p class="text-sm">70 RW</p>
+                <p class="text-sm">350 000 RW</p>
                 <button class="bg-teal-600 text-white hover:bg-red-800 py-2 px-6 rounded-md mt-2"><p class="text-xs">Buy Now</p></button>
             </div>
        </div>
@@ -204,18 +204,18 @@
   </div>
 </div>
 
-<div class="bg-white mb-20 px-4 sm:px8 lg:px-16 pt-10 sm:pt-16 lg:pt-20">
+<div class="bg-white mb-20 px-4 sm:px-8 lg:px-16 pt-10 sm:pt-16 lg:pt-20">
 <div class="flex flex-col lg:flex-row gap-16 lg:gap-16 ">
     <img src="../assets/photos/about.jpg" alt="about image"
-  class="  w-full max-w-[220px] sm: max-w-[280px] lg:max-w-[340px] rounded-xl lg:rounded-none mx-auto lg:mx-0 object-cover"
+  class="  w-full max-w-[220px] sm:max-w-[280px] lg:max-w-[340px] rounded-xl lg:rounded-none mx-auto lg:mx-0 object-cover"
   >
 <div class="">
-    <h3 class=" text-xl md:text-2xl lg:text-3xl font-bold text-gray-700 text-center lg:text-left">Cook Smarter. Live Better.</h3>
+    <h3 class=" text-xl md:text-2xl lg:text-3xl font-bold text-gray-700 text-center md:text-left">Cook Smarter. Live Better.</h3>
   <div class="mt-10 text-sm lg:text-lg text-center md:text-left">
     <p >At D Shop, we believe every home deserves quality kitchen equipment that makes </p> <p class="mt-2"> cooking enjoyable and effortless. From premium cookware and modern appliances to everyday</p><p class="mt-2">utensils, we carefully  select products that combine durability, style, and performance.</p>
     <p class="mt-8 lg:mt-14">Whether you're a beginner or a professional chef, we're here to help you</p><p> create delicious meals with confidence.</p>
 
-    <button class="bg-red-800 text-white py-3 px-8 rounded-md mt-17 hover:bg-red-700 "><RouterLink to="/about" class="text-sm">Learn More About Us</RouterLink></button>
+    <button class="bg-red-800 text-white py-3 px-8 rounded-md mt-16 hover:bg-red-700 "><RouterLink to="/about" class="text-sm">Learn More About Us</RouterLink></button>
   </div>
 </div>
 </div>
