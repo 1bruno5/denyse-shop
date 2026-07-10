615efa6f9908c4717d549ed301cfbfa6ea888ac9
1bruno5 <abakundanyeb@gmail.com>
2026-07-10 12:55:28 +0200

    input direction handling

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="font-sans">
-        <div class="bg-teal-800 h-[260px]">
-         <h1 class="text-white font-bold text-2xl md:text-3xl lg:text-5xl ml-4 md:ml-8 lg:ml-12 pt-2 md:pt-4 lg:pt-12">Contact Us</h1>
+        <div class="bg-teal-800 h-[220px] md:h-[260px]">
+         <h1 class="text-white font-bold text-2xl md:text-3xl lg:text-5xl ml-4 md:ml-8 lg:ml-12 pt-4 md:pt-4 lg:pt-12">Contact Us</h1>
          <p class="text-white text-sm md:text-base lg:text-xl ml-4 md:ml-8 lg:ml-12 pt-2 md:pt-6">Have questions about our D Shop? We're here to help you find the perfect <br> tools for your culinary needs.</p>
     </div>
     <div class="flex flex-col lg:flex-row bg-gray-100 gap-18">
          <form class="bg-white border border-gray-200 w-full max-w-[580px] ml-2 md:ml-4  mt-4 md:mt-8 mb-4 md:mb-8 pb-4 md:pb-12 pt-4 md:pt-8">
-                 <div class="ml-10 space-y-1">
+                 <div class="ml-1 md:ml-10 space-y-1">
                         <h3 class="">Send us a message</h3>
                         <p class="text-gray-600">Fill out the form below and we'll get back to you<br> within 24 hours.</p>
                         <p>Full Name</p>
