c609be6fc0f334875a8abdcea55202e1285fc9d9
1bruno5 <abakundanyeb@gmail.com>
2026-07-12 13:44:13 +0200

    form

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -39,14 +39,14 @@
                 </div>
                 <div class="flex flex-col items-center space-y-2">
                    <div class="flex sm:flex-row gap-3 sm:gap-6">
-                   <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full h-[40px] px-1 md:px-6 ">
-                   <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full h-[40px] px-1 md:px-6">
+                   <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[100px] h-[40px] px-1 md:px-6 ">
+                   <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[100px] h-[40px] px-1 md:px-6">
                    </div>
                    <br>
-                 <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[200px] md:max-w-[500px] h-[40px] p-4"><br>
-                 <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[200px] md:max-w-[500px] h-[40px] p-4"><br>
-                 <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[200px] md:max-w-[500px] h-[40px] p-4"><br>
-                 <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[200px] md:max-w-[500px] h-[120px] p-4 "><br>
+                 <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[600px] md:max-w-[500px] h-[40px] p-4"><br>
+                 <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[600px] md:max-w-[500px] h-[40px] p-4"><br>
+                 <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[600px] md:max-w-[500px] h-[40px] p-4"><br>
+                 <input type="text" placeholder="Enter your first name" class="border border-gray-600 rounded-lg w-full max-w-[600px] md:max-w-[500px] h-[120px] p-4 "><br>
                 </div>
               </form>
             </div>
