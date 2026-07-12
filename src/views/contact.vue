a247992d7c01e24c59d2dbea7ebee8f4ddf62019
1bruno5 <abakundanyeb@gmail.com>
2026-07-12 19:09:04 +0200

    social media and google maps added

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -9,7 +9,7 @@
         <img src="../assets/photos/icons/map.png" alt="map" class="w-8">
             <div class="text-center">
               <h1 class="font-bold  ">Visit Us</h1>
-              <p class="text-gray-600"> KN 4 Ave, Nyarungenge</p>
+              <p class="text-gray-600"> 8 KN 4 AVE, Kigali, Rwanda</p>
               <p class="text-gray-600">Kigali, Rwanda</p>
             </div>
       </div>
@@ -25,12 +25,12 @@
         <img src="../assets/photos/icons/mail.png" alt="map" class="w-8">
               <div class="text-center ">
                 <h1 class="font-bold">Email Us</h1>
-                <p class="text-gray-600"> KN 4 Ave, Nyarungenge</p>
-                <p class="text-gray-600">Kigali, Rwanda</p>
+                <p class="text-gray-600">umuhiredeny@gmail.com</p>
+                <p class="text-gray-600"></p>
               </div>
       </div>
     </div>
-    <div class="flex flex-col md:flex-row">
+    <div class="flex flex-col md:flex-row gap-0 md:gap-10">
             <div class="w-full max-w-[580px] md:max-w-[600px] mb-4">
               <form class="bg-white rounded-2xl shadow-xl mx-3 pt-6 md:pt-12 ">
                 <div class="px-4 md:px-10 mb-4 md:mb-8 space-y-2">
@@ -46,9 +46,34 @@
                  <input type="email" placeholder="Enter your email" class="border-2 border-gray-200 rounded-xl w-full max-w-[280px] md:max-w-[500px] h-[50px]  p-4 focus:border-teal-500 focus:outline-none" required><br>
                  <input type="tel" placeholder="Enter your phone number" class="border-2 border-gray-200 rounded-xl w-full max-w-[280px] md:max-w-[500px] h-[50px]  p-4 focus:border-teal-500 focus:outline-none"><br>
                  <textarea name="text area" id="" placeholder="Tell us how can we help you" rows="3" class="border-2 border-gray-200 w-full max-w-[280px] md:max-w-[500px] rounded-xl w-full p-8 resize-none focus:border-teal-500 focus:outline-none"></textarea><br>
-                 <button class="bg-teal-700 rounded-xl w-full max-w-[280px] md:max-w-[500px] h-[50px] text-white text-sm md:text-base font-bold mb-12 md:mb-20">Send  Message</button>
+                 <button class="bg-teal-700 rounded-xl w-full max-w-[280px] md:max-w-[500px] h-[50px] text-white text-sm md:text-base font-bold mb-10 md:mb-16">Send  Message</button>
                 </div>
               </form>
+            </div>
+            <div class="w-full max-w-[260px] md:max-w-[600px] flex flex-col gap-40">
+              <div class="bg-gray-100 shadow-md w-full  h-[280px] rounded-xl ">
+                <div class="flex flex-col items-center justify-center gap-6 md:gap-8 mt-6 md:mt-16">
+                  <img src="../assets/photos/icons/google-maps.png" alt="map" class="w-8">
+                  <div class="text-center">
+                   <h1>D Shop Kigali</h1>
+                   <p class="text-gray-600">8 KN 4 AVE, Kigali, Rwanda</p>
+                   <button class="bg-teal-700  mt-2 md:mt-4 rounded-lg text-white px-4 md:px-8 py-2"><a href="https://www.google.com/maps/@-1.9461077,30.0591942,3a,21.4y,177.77h,94.16t/data=!3m10!1e1!3m8!1svruk-dKwDJvkkVgFFAnNaw!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-4.158099062968034%26panoid%3Dvruk-dKwDJvkkVgFFAnNaw%26yaw%3D177.77071647829928!7i16384!8i8192!9m2!1b1!2i48?entry=ttu&g_ep=EgoyMDI2MDcwOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">Get Directions</a></button>
+                  </div>
+                </div>
+            </div>
+            <div class="bg-teal-900 w-full h-[180px] rounded-xl text-white">
+              <div class="px-4 md:px-6 py-4 md:py-8">
+               <h1 class="text-base font-bold">Follow Us</h1>
+               <p>Stay updated with new arrivals, deals, and automotive news.</p>
+               <div class="flex gap-6 mt-4 md:mt-10">
+                <img src="../assets/photos/icons/facebook.png" alt="facebook" class="w-6">
+                <button><a href="https://www.instagram.com/denyse_shop_makuzaload" target="_blank"><img src="../assets/photos/icons/instagram.png" alt="instagram" class="w-6" target="_blank"></a></button>
+                <button><a href="https://www.tiktok.com/@denyse.shop" target="_blank"><img src="../assets/photos/icons/tiktok.png" alt="tik tok" class="w-6"></a></button>
+                <button><a href="https://www.youtube.com/@clezotv-d4f" target="_blank"><img src="../assets/photos/icons/youtube.png" alt="youtube" class="w-6"></a></button>
+                <img src="../assets/photos/icons/linkedin.png" alt="linkedin" class="w-6">
+               </div>
+              </div>
+            </div>
             </div>
     </div>
 </div>
