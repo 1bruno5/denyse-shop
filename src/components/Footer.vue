53dd16c6a2e92b0d569d98676abb301fff80c2f6
1bruno5 <abakundanyeb@gmail.com>
2026-07-20 13:17:32 +0200

    footer changed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-gray-900 text-white">
+  <footer class="bg-gray-200 text-gray-500">
 
     <div class="max-w-7xl mx-auto px-6 py-12">
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
@@ -7,7 +7,7 @@
           <h1 class="text-3xl font-bold mb-4">
             D Shop
           </h1>
-          <p class="text-gray-300 text-sm leading-7">
+          <p class="text-gray-500 text-sm">
             D Shop is your trusted destination for premium kitchen
             equipment, cookware, and appliances. We provide quality
             products that make cooking easier, smarter, and more
@@ -15,29 +15,14 @@
           </p>
         </div>
 
-        <div>
-          <h2 class="text-xl font-semibold mb-4">
-            Contact Info
-          </h2>
 
-          <ul class="space-y-3 text-sm text-gray-300">
-            <li>+250 782 169 036</li>
-            <li>+250 788 839 489</li>
-            <li>info@dshop.com</li>
-            <li>Kigali, Rwanda</li>
-          </ul>
-        </div>
 
         <div>
           <h2 class="text-xl font-semibold mb-4">
             Quick Links
           </h2>
 
-          <ul class="space-y-3 text-sm text-gray-300">
-            <li>
-                <RouterLink to="/" class="hover:text-red-800">Home</RouterLink>
-            </li>
-
+          <ul class="space-y-3 text-sm text-gray-500">
             <li>
                    <RouterLink to="/products" class="hover:text-red-800">Products</RouterLink>
             </li>
@@ -52,39 +37,57 @@
           </ul>
         </div>
 
+                <div>
+          <h2 class="text-xl font-semibold mb-4">
+            Contact Info
+          </h2>
+
+          <ul class="space-y-3 text-sm text-gray-500">
+            <div class="flex">
+              <img src="../assets/photos/icons/telephone1.png" alt="phone" class="w-5">
+              <li class="ml-1">+250 782 169 036</li>
+            </div>
+            <div class="flex">
+              <img src="../assets/photos/icons/telephone1.png" alt="phone" class="w-5">
+              <li class="ml-1">+250 788 839 489</li>
+            </div>
+            <div class="flex">
+              <img src="../assets/photos/icons/envelope.png" alt="email" class="w-4">
+              <a href="https://mail.google.com/mail/u/0/#search/umuhiredeny%40gmail.com" target="_blank"><li class="ml-2">umuhiredeny@gmail.com</li></a>
+            </div>
+          </ul>
+        </div>
+
         <div>
           <h2 class="text-xl font-semibold mb-4">
             Follow Us
           </h2>
 
-          <ul class="space-y-3 text-sm text-gray-300">
-            <li>
-              <a href="#" class="hover:text-red-600 transition">
-                Facebook
+          <ul class="space-y-3 text-sm text-gray-500">
+            <li class="flex gap-1">
+              <img src="../assets/photos/icons/youtube1.png" alt="youtube" class="w-5">
+              <a href="https://www.youtube.com/@clezotv-d4f" target="_blank" class="hover:text-red-600 transition">
+                Youtube
               </a>
             </li>
 
-            <li>
-              <a href="#" class="hover:text-red-600 transition">
+            <li class="flex gap-1">
+              <img src="../assets/photos/icons/instagram1.png" alt="instagram" class="w-4">
+              <a href="https://www.instagram.com/denyse_shop_makuzaload" target="_blank" class="hover:text-red-600 transition">
                 Instagram
               </a>
             </li>
 
-            <li>
-              <a href="#" class="hover:text-red-800 transition">
-                X (Twitter)
-              </a>
-            </li>
-
-            <li>
-              <a href="#" class="hover:text-red-600 transition">
-                LinkedIn
+            <li class="flex gap-1">
+              <img src="../assets/photos/icons/video.png" alt="tik tok" class="w-5">
+              <a href="https://www.tiktok.com/@denyse.shop" target="_blank" class="hover:text-red-800 transition">
+                Tik Tok
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div class="border-t border-gray-700 mt-10 pt-6 text-center text-sm text-gray-400">
+      <div class="border-t border-gray-700 mt-10 pt-4 text-center text-sm text-gray-600">
         © 2026 D Shop. All Rights Reserved.
       </div>
 
